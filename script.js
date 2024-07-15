55963bd3855c541bf68bb5528a1b028723816d4e
abhii9988 <151422503+abhii9988@users.noreply.github.com>
2024-07-15 23:52:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+    document.getElementById('start-button').addEventListener('click', function() {
+        window.location.href = 'page1.html'
+    });
+    document.getElementById('next-button').addEventListener('click', function() {
+        window.location.href = 'memory.html'});
+         document.getElementById('back-button').addEventListener('click', function() {
+        window.location.href = 'lastpage.html'});
+</script>
